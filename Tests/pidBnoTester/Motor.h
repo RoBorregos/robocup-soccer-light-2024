fe05e1eeb5b4d590decce8b47ab47ffe8030a84e
maria ramos <marialramosc23@gmail.com>
2024-03-22 16:56:52 -0600

    Tester PID-BNO with movement

---
+++ Tests/pidBnoTester/Motor.h
@@ -0,0 +1,26 @@
+#ifndef Motor_h_
+#define Motor_h_
+
+#include "Arduino.h" 
+#pragma once
+
+class Motor {
+ public:
+  Motor();
+
+  void set(int speed, int pin1, int pin2); 
+  void startMotor();
+  void stop();
+  void motorFrontward();
+  void motorBackward(); 
+  int getMotorSpeed();
+  int getPin1();
+  int getPin2();
+
+ private:
+  int speed_;
+  int pin1_; 
+  int pin2_; 
+};
+
+#endif  

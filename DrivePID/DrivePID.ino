e89307e639016ea397bf91a0816cf14afa86ef00
marialramosc <a00838813@tec.mx>
2024-04-02 00:08:44 -0600

    Correction on method of the lineal movement PID

--- DrivePID/DrivePID.ino
+++ DrivePID/DrivePID.ino
@@ -5,11 +5,11 @@
 #include "Drive.h" 
 #include "PID.h"
 
-Drive robot_drive(4, 23, 22, 5, 24, 25, 6, 26, 27); 
+Drive robot_drive(6, 27, 26, 5, 25, 24, 4, 22, 23); 
 BNO orientation_sensor;  
-PID pid(1.2, 0, 0);  
-int angle_tester = 45;  
-int speed_tester = 120; 
+PID pid(0.9, 0, 0.09);  
+int angle_tester = 0;  
+int speed_tester = 150; 
 
 void setup (){ 
     Serial.begin(9600); 
@@ -28,4 +28,4 @@ void loop() {
 
     delay(50); 
 
-}
+} 

c87909beb4400fcb8dc9c30cac41bbca1972987f
marialramosc <a00838813@tec.mx>
2024-04-01 22:46:05 -0600

    IR Ring angle and radio detection

---
+++ IR-Ring/IRRing/IRRing.ino
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+#include "IR.h"
+
+IR ringIR;
+
+void setup() {
+  Serial.begin(115200);
+  unsigned long currentTime = millis();
+  ringIR.initiate(&currentTime);
+  ringIR.setOffset(0.0);
+}
+
+void loop() {
+  ringIR.updateData(); 
+
+  double angle = ringIR.getAngle();
+  double strength = ringIR.getStrength();
+  
+  Serial.print("angle: ");
+  Serial.print(angle);
+  Serial.print("\tradio: ");
+  Serial.println(strength);
+
+  delay(100); 
+} 

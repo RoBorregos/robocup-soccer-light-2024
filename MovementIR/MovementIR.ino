957f83ad60b37a3b9494804f0715f7e2b2dced48
marialramosc <a00838813@tec.mx>
2024-04-09 16:00:56 -0600

    Change in Method Following the Ball

--- MovementIR/MovementIR.ino
+++ MovementIR/MovementIR.ino
@@ -13,9 +13,19 @@ PID pid(0.9, 0, 0.09);
 IR ringIR;
 int speed_tester = 120;   
 unsigned long previous_time = 0;  
-int kFrequency = 50; 
+int kFrequency = 100; 
 double ballDistance = 0; 
-double ballAngle = 0; 
+double ballAngle = 0;  
+
+// Function to map the angle with offset
+double mapAngleWithOffset(double angle) {
+    angle += 7; // Add a 20-degree offset
+    if (angle >= 360) {
+        angle -= 360; // Wrap the angle back to the range of 0-359 degrees
+    }
+    return angle;
+}
+
 
 void setup (){ 
     Serial.begin(9600);  
@@ -36,10 +46,11 @@ void loop() {
     orientation_sensor.readValues();  
     
     ballAngle = (ballAngle < 0 ? 360 + ballAngle : ballAngle);   
-    ballAngle = 360 - ballAngle; 
+    ballAngle = 360 - ballAngle;  
+    ballAngle = mapAngleWithOffset(ballAngle);
 
-    Serial.print("ball angle:  "); 
-    Serial.print(ballAngle);
+    //Serial.print("ball angle:  "); 
+    //Serial.print(ballAngle);
     Serial.print("\tball distance: "); 
     Serial.println(ballDistance);   
 
@@ -47,7 +58,7 @@ void loop() {
       
       Serial.print("yaw: ");  
       double yaw = orientation_sensor.getYaw();
-      Serial.print(yaw); 
+      //Serial.print(yaw); 
 
       double control = pid.calculateError(yaw, 0); 
       searchBall(ballAngle, ballDistance, speed_tester, control); 
@@ -58,16 +69,21 @@ void loop() {
     }
       previous_time = time; 
   }  
-
 }  
 
 void searchBall(int ball_angle, int ball_distance, int speed, int error) {
-    if (ball_angle >= 350 && ball_angle <= 360) {
-        robot_drive.linealMovementError(0, speed, error);  
-    } else {
-        robot_drive.linealMovementError(ball_angle, speed, error);  
-    } 
-
+    if ((ball_angle >= 355 && ball_angle <= 360) || (ball_angle >= 0 && ball_angle <= 25 )) {
+        robot_drive.linealMovementError(0, 220, error);  
+    } else { 
+        if (ball_angle > 10 && ball_angle <= 175) {
+            ball_angle += 20;      
+        } else if (ball_angle >= 185 && ball_angle < 355) {
+            ball_angle -= 20;
+        }     
+        Serial.print("ball angle:  "); 
+        Serial.print(ball_angle);
+        robot_drive.linealMovementError(ball_angle, speed, error);
+    }
 } 
 
 

4835b5af304532b4f6d84f3e50c04233a069f02c
marialramosc <a00838813@tec.mx>
2024-04-14 17:04:48 -0600

    Libs For Valid Goal

---
+++ ValidGoal/PID.h
@@ -0,0 +1,20 @@
+#ifndef PID_h
+  #define PID_h
+
+class PID {
+  private:
+    double kP;
+    double kI;
+    double kD;
+    double control_error;
+    double previous_error;
+    double previous_time;
+    double sum_error; 
+
+  public:
+    PID(double p, double i, double d);
+    double calculateError(int angle, int set_point); 
+    
+};
+
+#endif

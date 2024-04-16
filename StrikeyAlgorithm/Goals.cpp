80b2dd684bb31b7651f0d89ff4c3231a71297fe5
marialramosc <a00838813@tec.mx>
2024-04-16 00:52:39 -0600

    Changes in Libs Goals

--- StrikeyAlgorithm/Goals.cpp
+++ StrikeyAlgorithm/Goals.cpp
@@ -1,8 +1,8 @@
 #include "Arduino.h"
-#include "Goals.h" 
+#include "Goals.h"
 
 Goals::Goals() {
-
+    numGoals = 0;
 }
 
 void Goals::initiate() {
@@ -10,36 +10,46 @@ void Goals::initiate() {
 }
 
 void Goals::updateData() {
-    pixy.ccc.getBlocks();  
-    if (pixy.ccc.numBlocks) {
-        x = pixy.ccc.blocks[0].m_x;
-        y = pixy.ccc.blocks[0].m_y;
-        width = pixy.ccc.blocks[0].m_width;
-        height = pixy.ccc.blocks[0].m_height;
-        color = pixy.ccc.blocks[0].m_signature;
+    pixy.ccc.getBlocks();
+    numGoals = pixy.ccc.numBlocks > 2 ? 2 : pixy.ccc.numBlocks;
+    for (uint8_t i = 0; i < numGoals; i++) {
+        goals[i].x = pixy.ccc.blocks[i].m_x;
+        goals[i].y = pixy.ccc.blocks[i].m_y;
+        goals[i].width = pixy.ccc.blocks[i].m_width;
+        goals[i].height = pixy.ccc.blocks[i].m_height;
+        goals[i].color = pixy.ccc.blocks[i].m_signature;
     }
 }
 
-bool Goals::detected() {
-    return pixy.ccc.numBlocks > 0;
+bool Goals::detected(uint8_t color) {
+    for (uint8_t i = 0; i < numGoals; i++) {
+        if (goals[i].color == color) {
+            return true;
+        }
+    }
+    return false;
 }
 
-uint16_t Goals::getX() {
-    return x;
+uint16_t Goals::getX(uint8_t index) {
+    return goals[index].x;
 }
 
-uint16_t Goals::getY() {
-    return y;
+uint16_t Goals::getY(uint8_t index) {
+    return goals[index].y;
 }
 
-uint16_t Goals::getWidth() {
-    return width;
+uint16_t Goals::getWidth(uint8_t index) {
+    return goals[index].width;
 }
 
-uint16_t Goals::getHeight() {
-    return height;
+uint16_t Goals::getHeight(uint8_t index) {
+    return goals[index].height;
 }
 
-uint8_t Goals::getColor() {
-    return color;
+uint8_t Goals::getColor(uint8_t index) {
+    return goals[index].color;
+}
+
+uint8_t Goals::getNumGoals() {
+    return numGoals;
 }
--- StrikeyAlgorithm/Goals.h
+++ StrikeyAlgorithm/Goals.h
@@ -1,25 +1,28 @@
 #ifndef Goals_h
-  #define Goals_h  
+#define Goals_h
 
 #include <Pixy2UART.h>
 
 class Goals {
   private:
     Pixy2UART pixy;
-    uint16_t x, y, width, height;
-    uint8_t color; 
+    struct GoalData {
+        uint16_t x, y, width, height;
+        uint8_t color;
+    } goals[2];
+    uint8_t numGoals;
 
   public:
-
     Goals(); 
     void initiate(); 
     void updateData(); 
-    bool detected(); 
-    uint16_t getX();
-    uint16_t getY();
-    uint16_t getWidth();
-    uint16_t getHeight(); 
-    uint8_t getColor(); 
+    bool detected(uint8_t color); 
+    uint16_t getX(uint8_t index);
+    uint16_t getY(uint8_t index);
+    uint16_t getWidth(uint8_t index);
+    uint16_t getHeight(uint8_t index); 
+    uint8_t getColor(uint8_t index); 
+    uint8_t getNumGoals();  // Added this method
 };
 
 #endif

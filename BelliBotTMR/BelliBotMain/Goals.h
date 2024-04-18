6d97231701031df995c8237c3974f61c0a0333c8
marialramosc <a00838813@tec.mx>
2024-04-17 23:50:38 -0600

    Belli Bot TMR Main

---
+++ BelliBotTMR/BelliBotMain/Goals.h
@@ -0,0 +1,28 @@
+#ifndef Goals_h
+#define Goals_h
+
+#include <Pixy2UART.h>
+
+class Goals {
+  private:
+    Pixy2UART pixy;
+    struct GoalData {
+        uint16_t x, y, width, height;
+        uint8_t color;
+    } goals[2];
+    uint8_t numGoals;
+
+  public:
+    Goals(); 
+    void initiate(); 
+    void updateData(); 
+    bool detected(uint8_t color); 
+    uint16_t getX(uint8_t index);
+    uint16_t getY(uint8_t index);
+    uint16_t getWidth(uint8_t index);
+    uint16_t getHeight(uint8_t index); 
+    uint8_t getColor(uint8_t index); 
+    uint8_t getNumGoals();  // Added this method
+};
+
+#endif
